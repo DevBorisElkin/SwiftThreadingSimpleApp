a770a685c5d2aa5190bfdf0e1c7f7c373157facb
Boris <ea.foreverrr@gmail.com>
2022-05-09 23:54:22 +0300

    Initial Commit

---
+++ SwiftThreadingSimpleApp/SwiftThreadingSimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThreadingSimpleAppApp.swift
+//  SwiftThreadingSimpleApp
+//
+//  Created by test on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThreadingSimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
